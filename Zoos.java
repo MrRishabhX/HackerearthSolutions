7e2bcc06858cc307be62a575e5286f9369af9514
MrRishabhX <86104065+MrRishabhX@users.noreply.github.com>
2021-07-08 21:50:02 +0530

    Update Zoos.java

--- Zoos.java
+++ Zoos.java
@@ -4,12 +4,14 @@ Determine if the entered word is similar to word zoo.
 
 For example, words such as zzoooo and zzzoooooo are similar to word zoo but not the words such as zzooo and zzzooooo.
 
-Input format
+Input format:
 First line: A word that starts with several Zs and continues by several Os.
 Note: The maximum length of this word must be 20.
 
-Output format
+Output format:
 Print Yes if the input word can be considered as the string zoo otherwise, print No.
+Input:zzzoooooo
+Output:Yes
 Question Link:https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/is-zoo-f6f309e7/
 */
 
