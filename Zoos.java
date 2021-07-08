7b78a865cc13861c529660b2255879eed7fc3583
MrRishabhX <86104065+MrRishabhX@users.noreply.github.com>
2021-07-08 21:40:30 +0530

    Create Zoos.java

---
+++ Zoos.java
@@ -0,0 +1,48 @@
+/*You are required to enter a word that consists of x and y that denote the number of Zs and Os respectively. The input word is considered similar to word zoo if 2×x=y.
+
+Determine if the entered word is similar to word zoo.
+
+For example, words such as zzoooo and zzzoooooo are similar to word zoo but not the words such as zzooo and zzzooooo.
+
+Input format
+First line: A word that starts with several Zs and continues by several Os.
+Note: The maximum length of this word must be 20.
+
+Output format
+Print Yes if the input word can be considered as the string zoo otherwise, print No.*/
+
+import java.io.*;
+class palindrome
+{
+    public static void main(String args[])throws IOException
+    {
+        InputStreamReader read=new InputStreamReader(System.in);
+        BufferedReader in= new BufferedReader(read);
+        String s1,s2="";int i,L;
+        s1=in.readLine();
+        L=s1.length();int c=0;int c1=0;
+        if(L<20)
+        {
+        for(i=0;i<L;i++)
+        {
+            if(s1.charAt(i)=='z')
+            {
+                c++;
+            }
+            if(s1.charAt(i)=='o')
+            {
+                c1++;
+            }
+        }
+    }
+    if(2*c==c1)
+    {
+        System.out.println("Yes");    
+    
+    }
+    else
+    {
+        System.out.println("No");
+    }
+}
+}
